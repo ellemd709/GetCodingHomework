d89b80bb8f31342a0f9c193f149eb09b9cdd5e11
Lisa Doody <101128408+ellemd709@users.noreply.github.com>
2022-06-09 23:19:59 +0000

    added more examples

--- script.js
+++ script.js
@@ -18,3 +18,21 @@ console.log(isEqualTo100(0,85));
 console.log(isEqualTo100(80,20));
 console.log(isEqualTo100(50,50));
 
+const getFileExtension = (str) => str.slice(str.lastIndexOf('.'));
+
+ console.log(getFileExtension('index.html'));
+console.log(getFileExtension('webpack.config.js'));
+
+
+
+
+const formatDate = (date = new Date()) => {
+const days = date.getDate() + 1;
+const months = date.getMonth() + 1;
+const years = date.getFullYear();
+return '${days}/${months}/${years}';
+  
+}
+console.log(formatDate());
+
+//* again getting the dollar signs and {} *//
